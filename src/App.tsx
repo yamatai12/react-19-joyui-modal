754af77e841331fe630608b53ec97539bbc27505
山根大生 <123088250+yamatai12@users.noreply.github.com>
2025-06-26 16:10:45 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import {
+  Modal,
+  ModalDialog,
+  ModalClose,
+  DialogTitle,
+  DialogContent,
+} from "@mui/joy";
+
+export default function App() {
+  return (
+    <Modal open={true}>
+      <div>sample</div>
+    </Modal>
+  );
+}
